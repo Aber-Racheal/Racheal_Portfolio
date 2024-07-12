070055322ab016199364f14842f40ec619381223
Aber-Racheal <rachealaberr@gmail.com>
2024-07-12 08:42:53 +0300

    book cover

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import './Footer.css'
+// import React from 'react'
+// import './Footer.css'
 
-const Footer = () => {
+// const Footer = () => {
 
-<div>
-    return(
-        <div>
+// <div>
+//     return(
+//         <div>
 
-        <div className='line'></div> 
+//         <div className='line'></div> 
 
-        <h1 className='contact'>Contacts</h1>
-        <h2>I Will Be Glad To Hear From You</h2>
-            <div className='footerContent'>
+//         <h1 className='contact'>Contacts</h1>
+//         <h2>I Will Be Glad To Hear From You</h2>
+//             <div className='footerContent'>
 
-        <div><p>+256787635823|+2567410923</p></div>
-        <div><p> LinkedIn</p></div>
-        <div> <p> Twitter</p></div>
+//         <div><p>+256787635823|+2567410923</p></div>
+//         <div><p> LinkedIn</p></div>
+//         <div> <p> Twitter</p></div>
 
-            </div>
-        </div>
-    )
-</div>
-}
-export default Footer
+//             </div>
+//         </div>
+//     )
+// </div>
+// }
+// export default Footer
